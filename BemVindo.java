2ea3f3135a057e6a1d67d019fd4175502481ef1d
Nicolas <nicolas.ribeirorstile@gmail.com>
2021-08-10 00:26:28 -0300

    Adicionando um commit a o arquivo OlaMundo.java

---
+++ BemVindo.java
@@ -0,0 +1,7 @@
+public class BemVindo{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Bem vindo aos testes git");
+    }
+}
---
+++ OlaGit.java
@@ -0,0 +1,7 @@
+public class OlaGit{
+
+    public static void main(String[] args) {
+
+        System.out.println("Iniciando os testes git");
+    }
+}
